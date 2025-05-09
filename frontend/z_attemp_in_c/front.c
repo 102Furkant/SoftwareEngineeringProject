688d550d64f0d25508a192a8ad59a3b97a772f47
102Furkant <102furkan116t@gmail.com>
2025-05-09 17:21:54 +0300

    Create front.c

---
+++ frontend/z_attemp_in_c/front.c
@@ -0,0 +1,32 @@
+// c de frontend başlangıç
+
+
+
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 900
+#define SCREEN_HEIGHT 900
+#define COLOR_WHITE 0xffffffff
+#define COLOR_BALCK 0x00000000
+#define COLOR_BLUE 0x34c3eb
+
+int main(){
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Liquid Simulation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+    SDL_Rect rectangle = (SDL_Rect){50,50,100,50};
+    SDL_FillRect(surface, &rectangle, COLOR_WHITE);
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Rect blue_rectangle = (SDL_Rect){150,50,100,50};
+    SDL_FillRect(surface, &blue_rectangle, COLOR_BLUE);
+    SDL_UpdateWindowSurface(window);
+    
+    SDL_Delay(3000);
+
+
+}
