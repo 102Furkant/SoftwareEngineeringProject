89bf5fff25a79fac16294f3242898e5ae9e2ba5a
102Furkant <102furkan116t@gmail.com>
2025-05-24 20:42:12 +0300

    Update main.py
    verilen dosya yolları güncellendi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from simulation import FluidSimulator
-from ui import FluidUI
+from backend.simulation import FluidSimulator
+from frontend.python.ui import FluidUI
 
 def main():
     simulator = FluidSimulator(nx=65, ny=65, dx=1.0, dt=0.1, viscosity=0.02, u_in=1.0)
