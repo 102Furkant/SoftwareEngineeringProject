99a25f9f0032ca700461dbbe86df46b0ad90b651
102Furkant <102furkan116t@gmail.com>
2025-05-26 13:09:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from backend.simulation import FluidSimulator
 from frontend.python.ui import FluidUI
 
 def main():
-    simulator = FluidSimulator(nx=65, ny=65, dx=1.0, dt=0.1, viscosity=0.02, u_in=1.0)
-    ui = FluidUI(simulator, width=800, height=800)
+    simulator = FluidSimulator(nx=80, ny=60, dx=1.0, dt=0.1, viscosity=0.02, u_in=1.0)
+    ui = FluidUI(simulator, width=1200, height=800)
     ui.run()
 
 if __name__ == "__main__":
